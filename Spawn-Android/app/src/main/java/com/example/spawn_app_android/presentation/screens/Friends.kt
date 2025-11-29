4e803dd29ba513fba87055064b209080f0fc93ec
Ethan Dsouza <edsouz01@student.ubc.ca>
2025-11-29 01:29:36 -0800

    #15 Cleanup

--- Spawn-Android/app/src/main/java/com/example/spawn_app_android/presentation/screens/Friends.kt
+++ Spawn-Android/app/src/main/java/com/example/spawn_app_android/presentation/screens/Friends.kt
@@ -1,7 +1,6 @@
 package com.example.spawn_app_android.presentation.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,20 +11,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.spawn_app_android.presentation.navigation.FriendsList
 import com.example.spawn_app_android.presentation.theme.SpawnAppAndroidTheme
-import com.mapbox.maps.extension.style.expressions.dsl.generated.all
-import com.mapbox.maps.extension.style.expressions.dsl.generated.mod
 
 // preview
 @OptIn(ExperimentalMaterial3Api::class)
